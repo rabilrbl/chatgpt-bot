1c4daa9d532f4e67d6c2d4c291c466b5f4b4506d
Mohammed Rabil <rabil@techie.com>
2023-12-25 07:59:42 +0530

    Add AuthorizedUserFilter for command handlers and message handler

--- chatgpt_bot/bot.py
+++ chatgpt_bot/bot.py
@@ -2,6 +2,7 @@ import os, asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from chatgpt_bot.handlers import start, help_command, handle_message, newchat_command, init_chatgpt
+from chatgpt_bot.filters import AuthorizedUserFilter
 
 def start_bot():
     loop = asyncio.get_event_loop()
@@ -9,15 +10,12 @@ def start_bot():
     app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
     
     # Commands
-    app.add_handlers(handlers=[
-        # Commands
-        CommandHandler("start", start),
-        CommandHandler("help", help_command),
-        CommandHandler("new", newchat_command),
-        
-        # Messages
-        MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
-    ])
+    app.add_handler(CommandHandler("start", start, filters=AuthorizedUserFilter()))
+    app.add_handler(CommandHandler("help", help_command, filters=AuthorizedUserFilter()))
+    app.add_handler(CommandHandler("new", newchat_command, filters=AuthorizedUserFilter()))
+    
+    # Messages
+    app.add_handler(MessageHandler( AuthorizedUserFilter() & ~filters.COMMAND & filters.TEXT, handle_message))
     
     # Run the bot until the user presses Ctrl-C
     app.run_polling(allowed_updates=Update.ALL_TYPES)
